df13eca1639a9379eb2ac8e491ec6ab5b93b8564
stefanprodan <stefan.prodan@gmail.com>
2020-01-16 15:35:20 +0200

    Update tools kubectl v1.17.0 kustomize v3.5.4 helmv3 v3.0.2 conftest v0.16.0 go v1.13.5

--- src/install.sh
+++ src/install.sh
@@ -2,13 +2,13 @@
 
 set -o errexit
 
-KUBECTL=1.16.2
+KUBECTL=1.17.0
 echo "downloading kubectl ${KUBECTL}"
 curl -sL https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL}/bin/linux/amd64/kubectl \
 -o /usr/local/bin/kubectl && chmod +x /usr/local/bin/kubectl
 kubectl version --client
 
-KUSTOMIZE=3.4.0
+KUSTOMIZE=3.5.4
 echo "downloading kustomize ${KUSTOMIZE}"
 curl -sL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE}/kustomize_v${KUSTOMIZE}_linux_amd64.tar.gz | \
 tar xz && mv kustomize /usr/local/bin/kustomize
@@ -20,7 +20,7 @@ curl -sSL https://get.helm.sh/helm-v${HELM_V2}-linux-amd64.tar.gz | \
 tar xz && mv linux-amd64/helm /usr/local/bin/helm && rm -rf linux-amd64
 helm version --client
 
-HELM_V3=3.0.0
+HELM_V3=3.0.2
 echo "downloading helm ${HELM_V3}"
 curl -sSL https://get.helm.sh/helm-v${HELM_V3}-linux-amd64.tar.gz | \
 tar xz && mv linux-amd64/helm /usr/local/bin/helmv3 && rm -rf linux-amd64
@@ -32,7 +32,7 @@ curl -sL https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL}/kub
 tar xz && mv kubeval /usr/local/bin/kubeval
 kubeval --version
 
-CONFTEST=0.15.0
+CONFTEST=0.16.0
 echo "downloading conftest ${CONFTEST}"
 curl -sL https://github.com/instrumenta/conftest/releases/download/v${CONFTEST}/conftest_${CONFTEST}_Linux_x86_64.tar.gz | \
 tar xz && mv conftest /usr/local/bin/conftest
