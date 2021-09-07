2630ba222cd5c401dbcf686f15dc47ac0f55d406
kskumgk63 <keisuke.umegaki.630@gmail.com>
2021-09-07 16:55:36 +0900

    fix KUBECTL

--- src/install.sh
+++ src/install.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-KUBECTL=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+KUBECTL=1.21.1
 echo "downloading kubectl ${KUBECTL}"
 curl -sL https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL}/bin/linux/amd64/kubectl \
 -o /usr/local/bin/kubectl && chmod +x /usr/local/bin/kubectl
