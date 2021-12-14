9fa7eb3dbcd449ef3d26452f81405d65ed8fb725
Stefan Prodan <stefan.prodan@gmail.com>
2021-12-14 15:56:07 +0200

    Update tools - kubectl 1.23.0 - kustomize 4.4.1 - helmv3 3.7.2 - kubeaudit 0.16.0 - kubeconform 0.4.12 - conftest 0.28.3 - go 1.17
    Signed-off-by: Stefan Prodan <stefan.prodan@gmail.com>

--- src/install.sh
+++ src/install.sh
@@ -2,13 +2,13 @@
 
 set -o errexit
 
-KUBECTL=1.21.1
+KUBECTL=1.23.0
 echo "downloading kubectl ${KUBECTL}"
 curl -sL https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL}/bin/linux/amd64/kubectl \
 -o /usr/local/bin/kubectl && chmod +x /usr/local/bin/kubectl
 kubectl version --client
 
-KUSTOMIZE=4.3.0
+KUSTOMIZE=4.4.1
 echo "downloading kustomize ${KUSTOMIZE}"
 curl -sL https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv${KUSTOMIZE}/kustomize_v${KUSTOMIZE}_linux_amd64.tar.gz | \
 tar xz && mv kustomize /usr/local/bin/kustomize
@@ -20,7 +20,7 @@ curl -sSL https://get.helm.sh/helm-v${HELM_V2}-linux-amd64.tar.gz | \
 tar xz && mv linux-amd64/helm /usr/local/bin/helm && rm -rf linux-amd64
 helm version --client
 
-HELM_V3=3.6.3
+HELM_V3=3.7.2
 echo "downloading helm ${HELM_V3}"
 curl -sSL https://get.helm.sh/helm-v${HELM_V3}-linux-amd64.tar.gz | \
 tar xz && mv linux-amd64/helm /usr/local/bin/helmv3 && rm -rf linux-amd64
@@ -32,13 +32,13 @@ curl -sL https://github.com/instrumenta/kubeval/releases/download/v${KUBEVAL}/ku
 tar xz && mv kubeval /usr/local/bin/kubeval
 kubeval --version
 
-KUBEAUDIT=0.14.2
+KUBEAUDIT=0.16.0
 echo "downloading kubeaudit ${KUBEAUDIT}"
-curl -sSL https://github.com/Shopify/kubeaudit/releases/download/v${KUBEAUDIT}/kubeaudit_${KUBEAUDIT}_linux_amd64.tar.gz | \
+curl -sSL https://github.com/Shopify/kubeaudit/releases/download/${KUBEAUDIT}/kubeaudit_${KUBEAUDIT}_linux_amd64.tar.gz | \
 tar xz && mv kubeaudit /usr/local/bin/kubeaudit
 kubeaudit --help
 
-CONFTEST=0.27.0
+CONFTEST=0.28.3
 echo "downloading conftest ${CONFTEST}"
 curl -sL https://github.com/open-policy-agent/conftest/releases/download/v${CONFTEST}/conftest_${CONFTEST}_Linux_x86_64.tar.gz | \
 tar xz && mv conftest /usr/local/bin/conftest
@@ -50,6 +50,12 @@ curl -sL https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUB
 -o /usr/local/bin/kubeseal && chmod +x /usr/local/bin/kubeseal
 kubeseal --version
 
+KUBECONFORM=0.4.12
+echo "downloading kubeconform ${KUBECONFORM}"
+curl -sSL https://github.com/yannh/kubeconform/releases/download/v${KUBECONFORM}/kubeconform-linux-amd64.tar.gz | \
+tar xz && mv kubeconform /usr/local/bin/kubeconform
+kubeconform --help
+
 echo "downloading yq"
 curl -sL https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 \
 -o /usr/local/bin/yq && chmod +x /usr/local/bin/yq
@@ -60,8 +66,3 @@ curl -sL https://github.com/stedolan/jq/releases/latest/download/jq-linux64 \
 -o /usr/local/bin/jq && chmod +x /usr/local/bin/jq
 jq --version
 
-KUBECONFORM="v0.4.12"
-echo "downloading kubeconform ${KUBECONFORM}"
-curl -sSL https://github.com/yannh/kubeconform/releases/download/${KUBECONFORM}/kubeconform-linux-amd64.tar.gz | \
-tar xz && mv kubeconform /usr/local/bin/kubeconform
-kubeconform --help
